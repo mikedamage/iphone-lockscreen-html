28dc1069743487628fa860e9873f510a1e652c8c
Mike Green <mike.is.green@gmail.com>
2009-03-24 23:09:22 -0400

    updated rss reader script to use an in-file ERB template

--- rss.rb
+++ rss.rb
@@ -17,28 +17,25 @@ require 'ostruct'
 require 'open-uri'
 require 'rss/1.0'
 require 'rss/2.0'
+require 'erb'
 
 defaults = {
 	:items => 5,
 	:details => false,
-	:link => false,
-	:html => false
+	:link => false
 }
 
-config = OpenStruct.new(defaults)
+@config = OpenStruct.new(defaults)
 
 opts = OptionParser.new
 opts.on('-n', '--number NUMBER', 'How many headlines to show (5 by default)') do |n|
-	config.items = n.to_i
+	@config.items = n.to_i
 end
 opts.on('-d', '--details', "Display the items' summaries along with their headlines") do
-	config.details = true
+	@config.details = true
 end
 opts.on('-l', '--link', "Display the item's URL beneath its title") do
-	config.link = true
-end
-opts.on('-x', '--xhtml', "Renders the headlines as an XHTML ordered list") do
-	config.html = true
+	@config.link = true
 end
 opts.on_tail('-h', '--help', "Display this help message") do
 	puts opts
@@ -49,44 +46,46 @@ opts.parse!(ARGV)
 url = ARGV.shift
 raw_feed = ""
 open(url) {|s| raw_feed = s.read }
-rss = RSS::Parser.parse(raw_feed, false)
-title_underline = ""
-i = 0
-while i <= (rss.channel.title.size + 3) do
-	title_underline += "="
-	i += 1
-end
+@rss = RSS::Parser.parse(raw_feed, false)
+@range = 0..(@config.items - 1)
+@template = ERB.new(DATA.read)
+puts @template.result(binding)
 
-if config.html
-	puts "<html>"
-	puts "<head>"
-	puts "<style type=\"text/css\">"
-	puts "body {color:#fff;font-family:sans-serif;}"
-	puts "ul {padding-left:none;}"
-	puts "</style>\n</head>\n<body>"
-	puts "<h2>#{rss.channel.title}</h1>"
-	puts "<ol>\n"
-else
-	puts rss.channel.title + "\n" + title_underline + "\n"
-end
+__END__
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-i = 0
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
-config.items.to_i.times do
-	if config.html
-		puts "<li><strong>#{rss.items[i].date.strftime("%m-%d-%Y")}:</strong> #{rss.items[i].title}"
-		puts "<ul>" if config.link or config.details
-		puts "<li><a href=\"#{rss.items[i].link}\">#{rss.items[i].link}</a></li>" if config.link
-		puts "<li>#{rss.items[i].description}</li>" if config.details
-		puts "</ul>" if config.link or config.details
-		puts "</li>"
-	else
-		puts rss.items[i].date.strftime("%m-%d-%Y: ") + " " + rss.items[i].title
-		puts rss.items[i].link if config.link
-		puts rss.items[i].description if config.details
-	end
-	puts "\n"
-	i += 1
-end
+	<title><%= @rss.channel.title %></title>
+	<style type="text/css">
+		body {
+			font-family:sans-serif;
+			font-size:8px;
+			color:#fff;
+			text-shadow:1px 1px 1px #000;
+		}
+		ul {
+			padding-left:0;
+			list-style:none;
+		}
+		ol {
+			padding-left:0;
+		}
+	</style>
+</head>
 
-puts "</ol>\n</body>\n</html>" if config.html
+<body>
+	<h1><%= @rss.channel.title %></h1>
+	<ol>
+		<% @range.to_a.each do |i| %>
+		<li>
+			<strong><%= @rss.items[i].date.strftime("%m-%d-%Y") %>:</strong> <%= @rss.items[i].title %>
+			<%= "<br/><a href=\"#{@rss.items[i].link}\">#{@rss.items[i].link}</a>" if @config.link %>
+		</li>
+		<% end %>
+	</ol>
+</body>
+</html>
